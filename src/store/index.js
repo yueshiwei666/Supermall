4203d209229fc2029807cb94a98cadcaa2d1e89d
yueshiwei666 <1467658005@qq.com>
2020-02-01 14:24:40 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+//在vue中使用vuex
 Vue.use(Vuex)
+//通过vuex创建一个vuex的实例
 const store = new Vuex.Store({
     state:{
         qqqq:['我是vuex保存的数据']
@@ -12,5 +13,5 @@ const store = new Vuex.Store({
         }
     } 
 })
-
+//把vuex的实例导出即可
 export default store  
