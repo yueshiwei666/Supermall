d6ca6605c1d6ea42e3f1c97cf6274a7a17f02d09
yueshiwei666 <1467658005@qq.com>
2020-03-24 00:13:18 +0800

    qqqqqqqqqq

--- src/store/vuex案例教程.js
+++ src/store/vuex案例教程.js
@@ -1,74 +1,70 @@
 //使用手册Vuex.Store({中有五个对象，      把本文件给改成index 文件形式，在购物车中有实例的案例
 //不要管那个报错了
-        //那个state是管理数据的，就是保存数据的
-        //mutations就是methods方法，不一样的是必须通过mutations来操作state中的数据
-                //不然直接在外部对数据做改变的话，state的数据的本身是不变的
-        //那个getters就是类似那个计算属性，
-        //另外两个不详   从136看起
+//那个state是管理数据的，就是保存数据的
+//mutations就是methods方法，不一样的是必须通过mutations来操作state中的数据
+//不然直接在外部对数据做改变的话，state的数据的本身是不变的
+//那个getters就是类似那个计算属性，
+//另外两个不详   从136看起
 
-
-        //在外部的使用方法：    一些参数的问题在他下部写，这里没写
-          //this.$store  就是访问的是 vuex这个总的空间了 
-          //this.$store.state.state中保存的数据   访问state中的数据
-          //this.commit('mutations中的定义的方法'，参数，参数)  访问mutations中的方法
-          //this.$store.在getters中的方法  不用加括号就可以调用的，类似计算属性   方法getters中的方法   
+//在外部的使用方法：    一些参数的问题在他下部写，这里没写
+//this.$store  就是访问的是 vuex这个总的空间了
+//this.$store.state.state中保存的数据访问state中的数据
+//this.commit('mutations中的定义的方法'，参数，参数)  访问mutations中的方法
+//this.$store.在getters中的方法  不用加括号就可以调用的，类似计算属性   方法getters中的方法
 //})
 
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 //在vue中使用vuex
-Vue.use(Vuex)
+Vue.use(Vuex);
 //通过vuex创建一个vuex的实例
 const store = new Vuex.Store({
-//在外部访问vuex时需要，{{$store.state.vuex中存放的变量名字}}
-                    //或者 this.$store.state.state中存放的名字
-//mtations是vuex中方法，就是vuex中需要写一些逻辑的函数，可以写在这个里面的
+  //在外部访问vuex时需要，{{$store.state.vuex中存放的变量名字}}
+  //或者 this.$store.state.state中存放的名字
+  //mtations是vuex中方法，就是vuex中需要写一些逻辑的函数，可以写在这个里面的
 
-    state:{
-        //在vuex中保存的数据，也是保存的状态
-        num:1,
-        num2:1,
-        test:['我是vuex保存的数据'],
-        student:[
-            {name:'王五',age:10},
-            {name:'张三',age:5}
-        ]
-
-    },
-    mutations:{// !!!强调这里的函数的第一个参数都是上面的state    
-         //第二个参数是你自定义的参数，在外部就引入             
-                               //commit == mutations
+  state: {
+    //在vuex中保存的数据，也是保存的状态
+    num: 1,
+    num2: 1,
+    test: ["我是vuex保存的数据"],
+    student: [
+      { name: "王五", age: 10 },
+      { name: "张三", age: 5 }
+    ]
+  },
+  mutations: {
+    // !!!强调这里的函数的第一个参数都是上面的state
+    //第二个参数是你自定义的参数，在外部就引入
+    //commit == mutations
     //使用方法：在外部需要 this.$store.commit('mutations中的方法的名字')==调用了一次vuex中的函数
     //vuex中写入的一些方法，方法中的第一个参数就是vuex中state
-        //在外部还有一种提交的风格  this.$store.commit({
-            //type:'这个事件的名字',
-            //参数   这个参数在store中的方法中打印就是一个对象  你先后台打印一下
-        //})
-        num(state,num){
-            state.num += num
-        },
-        addCart(state,payload){
-            state.num = state.num + payload
-        }
+    //在外部还有一种提交的风格  this.$store.commit({
+    //type:'这个事件的名字',
+    //参数   这个参数在store中的方法中打印就是一个对象  你先后台打印一下
+    //})
+    num(state, num) {
+      state.num += num;
     },
-    actions:{
-
-    },
-    getters:{
-        //类似那个计算属性  computed 实时来检测数据的变化
-        //这个里面第一个参数是state第二个参数是getters 不用加括号就是调用的
-        //他不接受第三个参数
-        //类似那个计算属性  computed 实时来检测数据的变化
-        filterage(state,getters){     //这个是过滤函数 s == state.student
-            return state.student.filter(s => s.age > 9)
-        },
-        push(){
-            return '--------------'
-        }
-    },
-    modules:{
-
+    addCart(state, payload) {
+      state.num = state.num + payload;
     }
-})
+  },
+  actions: {},
+  getters: {
+    //类似那个计算属性  computed 实时来检测数据的变化
+    //这个里面第一个参数是state第二个参数是getters 不用加括号就是调用的
+    //他不接受第三个参数
+    //类似那个计算属性  computed 实时来检测数据的变化
+    filterage(state, getters) {
+      //这个是过滤函数 s == state.student
+      return state.student.filter(s => s.age > 9);
+    },
+    push() {
+      return "--------------";
+    }
+  },
+  modules: {}
+});
 //把vuex的实例导出即可
-export default store  
+export default store;
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,254 +1,251 @@
 <template>
-    <div class="divv">
-        <nav-bar class="navbar"><!-- title的展示 -->
-            <div slot="left" class="left" @click='back'>
-                <img src="~assets/img/img/common/back.svg" alt="">
-            </div>
-            <div slot='center' class="title" >
-                <div class="titleItem"
-                     v-for='(value,index) in title'
-                     :key="index"
-                     :class='{active:currentindex == index}'
-                     @click='click(index)'>
-                    {{value}}
-                </div>
-            </div>
-        </nav-bar>
+  <div class="divv">
+    <nav-bar class="navbar">
+      <!-- title的展示 -->
+      <div slot="left" class="left" @click="back">
+        <img src="~assets/img/img/common/back.svg" alt />
+      </div>
+      <div slot="center" class="title">
+        <div
+          class="titleItem"
+          v-for="(value,index) in title"
+          :key="index"
+          :class="{active:currentindex == index}"
+          @click="click(index)"
+        >{{value}}</div>
+      </div>
+    </nav-bar>
 
-       
-        <!-- 返回顶部的按钮啊 -->
-        <basetop v-show="showtop"
-                 class="basetop"
-                 @click.native='basetops'></basetop>
+    <!-- 返回顶部的按钮啊 -->
+    <basetop v-show="showtop" class="basetop" @click.native="basetops"></basetop>
 
-        <!-- 购物车的展示 -->
-        <shopbar @addToCart='addToCartt'></shopbar>
+    <!-- 购物车的展示 -->
+    <shopbar @addToCart="addToCartt"></shopbar>
 
+    <scroll class="scrolll" ref="scroll" @detail="detailscroll">
+      <!-- 轮播图的展示 -->
+      <detailSwiper :data="slideshow" />
 
+      <!--轮播图下面基础信息-->
+      <datailbaseInfo :goods="goods" />
 
-        <scroll class="scrolll"
-                ref="scroll"
-                @detail='detailscroll'>
-                <!-- 轮播图的展示 -->
-            <detailSwiper :data='slideshow' />
-                
-            <!--轮播图下面基础信息-->
-            <datailbaseInfo :goods='goods' />
+      <!-- 商品 -->
+      <datailshop ref="shop" :shop="shop"></datailshop>
 
-            <!-- 商品 -->
-            <datailshop ref="shop" :shop='shop'></datailshop>
-            
-            <!-- 最后的文字和图片 -->
-            <datailgoods @image='image' :images='images' class="images"></datailgoods>
-            
-            <!-- 展示第二大项的参数的信息了 -->
-            <detailparams ref="params" :detailparams='goodsparam'></detailparams>
+      <!-- 最后的文字和图片 -->
+      <datailgoods @image="image" :images="images" class="images"></datailgoods>
 
-            <!--展示评论信息-->
-            <detailcommentInfo @img='img' ref="commentInfo" :detailcommentInfo='detailcommentInfo'></detailcommentInfo>
-            
-            <!-- 展示推荐的信息 -->
-            <goodss @imgLoad='imgLoad' ref="goodss" class="goodss" :goods='getdetailrecommend' ></goodss>
+      <!-- 展示第二大项的参数的信息了 -->
+      <detailparams ref="params" :detailparams="goodsparam"></detailparams>
 
-            
-            <br> <br> <br> <p></p>
-       </scroll>
-    </div>
+      <!--展示评论信息-->
+      <detailcommentInfo @img="img" ref="commentInfo" :detailcommentInfo="detailcommentInfo"></detailcommentInfo>
+
+      <!-- 展示推荐的信息 -->
+      <goodss @imgLoad="imgLoad" ref="goodss" class="goodss" :goods="getdetailrecommend"></goodss>
+
+      <br />
+      <br />
+      <br />
+      <p></p>
+    </scroll>
+  </div>
 </template>
 
 <script>
 import NavBar from "components/public/购物街";
-import scroll from 'plug-in/滚动的插件/BetterScroll.vue'
-import detailSwiper from './detailSwiper'  //轮播图的组件
-import datailbaseInfo from './detailbaseInfo'  //轮播图下面基础信息
-import datailshop from './detailshop'  //商品的数展示
-import datailgoods from './detailgoods'  //最后的图片的数据
-import detailparams from './detailparams'  
-import detailcommentInfo from './detailcommentInfo'
-import goodss from './goods.vue'
-import basetop from 'components/public/baseTop/baseTop.vue'
-import shopbar from './shopbar'
+import scroll from "plug-in/滚动的插件/BetterScroll.vue";
+import detailSwiper from "./detailSwiper"; //轮播图的组件
+import datailbaseInfo from "./detailbaseInfo"; //轮播图下面基础信息
+import datailshop from "./detailshop"; //商品的数展示
+import datailgoods from "./detailgoods"; //最后的图片的数据
+import detailparams from "./detailparams";
+import detailcommentInfo from "./detailcommentInfo";
+import goodss from "./goods.vue";
+import basetop from "components/public/baseTop/baseTop.vue";
+import shopbar from "./shopbar";
 /* 网络请求本页的数据 */
-import {getdetail,goods,shop,goodsparam,getdetailrecommend} from 'network/detail.js'
+import {
+  getdetail,
+  goods,
+  shop,
+  goodsparam,
+  getdetailrecommend
+} from "network/detail.js";
 export default {
-    name:'Detail',
-    data() {
-        return {
-            detail:'',
-            title:['商品','参数','评论','推荐'],
-            currentindex:0,
-            //轮播图的图片的保存
-            slideshow:[],
-            goods:{},
-            shop:{},
-            images:{},
-            goodsparam:{},
-            detailcommentInfo:{},
-            getdetailrecommend:[],
-            showtop:false,
-            tabbar:[]
+  name: "Detail",
+  data() {
+    return {
+      detail: "",
+      title: ["商品", "参数", "评论", "推荐"],
+      currentindex: 0,
+      //轮播图的图片的保存
+      slideshow: [],
+      goods: {},
+      shop: {},
+      images: {},
+      goodsparam: {},
+      detailcommentInfo: {},
+      getdetailrecommend: [],
+      showtop: false,
+      tabbar: []
+    };
+  },
+  components: {
+    NavBar,
+    scroll,
+    detailSwiper,
+    datailbaseInfo,
+    datailshop,
+    datailgoods,
+    detailparams,
+    detailcommentInfo,
+    goodss,
+    basetop,
+    shopbar
+  },
+  created() {
+    //这个params参数的意思固定的
+    //iid是router文件中index配置的路由的基本信息
+    this.detail = this.$route.params.iid;
+
+    /* 获取数据的展示 */
+    getdetail(this.detail)
+      .then(result => {
+        //为什么在这里写 let  const报错
+        var result = result; //老师在这个地方同步
+        //      ||  数据同步
+        this.slideshow = result.data.result.itemInfo.topImages;
+
+        //这里需要把轮播图下面的杂乱的数据整合到一个对象goods中goods中有
+        //已经整合好的数据了，之后就访问就可以了
+        var data = result.data.result; //同步到老师的数据中
+        console.log(data);
+
+        this.goods = new goods(
+          data.itemInfo,
+          data.columns,
+          data.shopInfo.services
+        );
+
+        this.shop = new shop(data.shopInfo);
+
+        this.images = data.detailInfo;
+
+        this.goodsparam = new goodsparam(
+          data.itemParams.info,
+          data.itemParams.rule
+        );
+
+        if (data.rate.cRate !== 0) {
+          //判断有没有值啊
+          this.detailcommentInfo = data.rate.list[0];
         }
+      })
+      .catch(err => {
+        alert("详情页面的数据获取失败");
+      });
+
+    //获取推荐的信息页面
+    getdetailrecommend()
+      .then(result => {
+        this.getdetailrecommend = result.data.data.list;
+      })
+      .catch(err => {
+        alert("推荐信息获取失败");
+      });
+
+    //这个this就是获取本地的vue就是本组件，
+    //nexttick就是在组件的东西都完成好之后就开始执行他内部的回调函数
+    this.$nextTick(() => {});
+  },
+  mounted() {
+    //获取事件的获取元素的一般最会写在mounted里面
+  },
+  methods: {
+    click(index) {
+      this.currentindex = index;
+      this.$refs.scroll.basetop(0, -this.tabbar[index], 200);
     },
-    components:{
-        NavBar,
-        scroll,
-        detailSwiper,
-        datailbaseInfo,
-        datailshop,
-        datailgoods,
-        detailparams,
-        detailcommentInfo,
-        goodss,
-        basetop,
-        shopbar
-        
+    back() {
+      //下面两个的方法都是返回的，go可以指定，第一个就是返回一个
+      //this.$router.back()  //返回一次
+      this.$router.go(-1);
     },
-    created() {
-                   //这个params参数的意思固定的  
-                   //iid是router文件中index配置的路由的基本信息
-        this.detail = this.$route.params.iid
-        
-        /* 获取数据的展示 */
-        getdetail(this.detail)
-        .then(result =>{            
-            //为什么在这里写 let  const报错
-            var result= result;       //老师在这个地方同步
-                                   //      ||  数据同步
-            this.slideshow = result.data.result.itemInfo.topImages;
-        
-            //这里需要把轮播图下面的杂乱的数据整合到一个对象goods中goods中有
-            //已经整合好的数据了，之后就访问就可以了
-            var data = result.data.result; //同步到老师的数据中
-            console.log(data)
-            
-            this.goods = new goods(data.itemInfo,data.columns,data.shopInfo.services);
-            
-            
-            this.shop = new shop(data.shopInfo)
+    detailscroll(location) {
+      let show = -location.y;
+      this.showtop = show > 1000;
 
-            this.images = data.detailInfo;
-
-            this.goodsparam = new goodsparam(data.itemParams.info,
-            data.itemParams.rule)
-
-            if(data.rate.cRate !== 0){ //判断有没有值啊
-                this.detailcommentInfo = data.rate.list[0]
-            }
-
-        })
-        .catch(err =>{
-            alert('详情页面的数据获取失败')
-        })
-
-        //获取推荐的信息页面
-        getdetailrecommend()
-        .then(result =>{
-            this.getdetailrecommend = result.data.data.list;
-           
-            
-        })
-        .catch(err =>{
-            alert('推荐信息获取失败')
-        })
-
-          //这个this就是获取本地的vue就是本组件，
-          //nexttick就是在组件的东西都完成好之后就开始执行他内部的回调函数
-        this.$nextTick(() =>{
-               
-               
-        })
-        
-    },
-    mounted() {
-        //获取事件的获取元素的一般最会写在mounted里面
-
-
-    },
-    methods: {
-        click(index){
-            this.currentindex = index;
-            this.$refs.scroll.basetop(0,-(this.tabbar[index]),200)
-
-        },
-        back(){
-            //下面两个的方法都是返回的，go可以指定，第一个就是返回一个
-            //this.$router.back()  //返回一次
-            this.$router.go(-1);
-        },
-        detailscroll(location){
-            let show = -(location.y);
-            this.showtop = show>1000
-
-            //this.tabbar 中保存的是各个组件对应顶部的位置信息
-            //完成滑动到哪个位置就让最上面的字变成相应的颜色
-            for(let i=0;i<this.tabbar.length; i++){
-                let length = this.tabbar.length;
-                //针对第一二三   元素进行的   因为第4个元素我是无法滑动的底部的所以需要单独的设置一下
-                if((this.currentindex !== i) && ( i< length-1 && this.tabbar[i] <= show && this.tabbar[i+1] > show ) 
-                                         || 
-                  ( i == length-1 && show >= this.tabbar[i])){ 
-                      console.log(i)
-                      this.currentindex =i; 
-                  }
-            }
-        },
-        basetops(){
-            this.$refs.scroll.basetop(0,0)
-        },
-        image(){
-            this.$refs.scroll.refresh();
-            //当只要有图片加载完成的时候就来刷新组件对应顶部的一个高度，
-            //并且把最新的高度保存到data中的数组中
-            this.refresh_scroll_height()
-        },
-        img(){
-            this.$refs.scroll.refresh();
-            //当只要有图片加载完成的时候就来刷新组件对应顶部的一个高度，
-            //并且把最新的高度保存到data中的数组中
-            this.refresh_scroll_height()
-        },
-        imgLoad(){
-            this.$refs.scroll.refresh();
-            //当只要有图片加载完成的时候就来刷新组件对应顶部的一个高度，
-            //并且把最新的高度保存到data中的数组中
-            this.refresh_scroll_height()
-        },
-        refresh_scroll_height(){
-            this.tabbar = []
-            //$el 就是拿到组件的根组件，offsetTop就是距离顶部的位置
-            this.tabbar.push(0)
-            this.tabbar.push(this.$refs.params.$el.offsetTop)
-            this.tabbar.push(this.$refs.commentInfo.$el.offsetTop)
-            this.tabbar.push(this.$refs.goodss.$el.offsetTop)
-            /* console.log(this.tabbar); */
-        },
-        addToCartt(){
-            console.log(this.goods);
-            
-            let product = {}
-            product.image = this.slideshow[0];
-            product.title = this.goods.title;
-            product.desc = this.goods.desc;
-            product.price = this.goods.newPrice
-            product.iid = this.detail;
-            product.trueprice = this.goods.realPrice;
-
-            //使用vuex把用户添加的购物车的那些东西都方法哦vuex数据中做保存
-            //用store中的actions中来调用方法
-            //this.$store.dispatch('addshop', product)  
-            this.$store.commit('addshop',product).then(result=>{
-                console.log(result);
-                
-            }) 
-              
-            
+      //this.tabbar 中保存的是各个组件对应顶部的位置信息
+      //完成滑动到哪个位置就让最上面的字变成相应的颜色
+      for (let i = 0; i < this.tabbar.length; i++) {
+        let length = this.tabbar.length;
+        //针对第一二三   元素进行的   因为第4个元素我是无法滑动的底部的所以需要单独的设置一下
+        if (
+          (this.currentindex !== i &&
+            i < length - 1 &&
+              this.tabbar[i] <= show &&
+              this.tabbar[i + 1] > show) ||
+          (i == length - 1 && show >= this.tabbar[i])
+        ) {
+          console.log(i);
+          this.currentindex = i;
         }
+      }
     },
-            //当页面只要一发生变化就执行函数updated中的内容的
-            //注意不只执行一次的
-    updated() {
+    basetops() {
+      this.$refs.scroll.basetop(0, 0);
+    },
+    image() {
+      this.$refs.scroll.refresh();
+      //当只要有图片加载完成的时候就来刷新组件对应顶部的一个高度，
+      //并且把最新的高度保存到data中的数组中
+      this.refresh_scroll_height();
+    },
+    img() {
+      this.$refs.scroll.refresh();
+      //当只要有图片加载完成的时候就来刷新组件对应顶部的一个高度，
+      //并且把最新的高度保存到data中的数组中
+      this.refresh_scroll_height();
+    },
+    imgLoad() {
+      this.$refs.scroll.refresh();
+      //当只要有图片加载完成的时候就来刷新组件对应顶部的一个高度，
+      //并且把最新的高度保存到data中的数组中
+      this.refresh_scroll_height();
+    },
+    refresh_scroll_height() {
+      this.tabbar = [];
+      //$el 就是拿到组件的根组件，offsetTop就是距离顶部的位置
+      this.tabbar.push(0);
+      this.tabbar.push(this.$refs.params.$el.offsetTop);
+      this.tabbar.push(this.$refs.commentInfo.$el.offsetTop);
+      this.tabbar.push(this.$refs.goodss.$el.offsetTop);
+      /* console.log(this.tabbar); */
+    },
+    addToCartt() {
+      console.log(this.goods);
 
-            /* //这里写这个代码也可以获取到各个组件对应的顶部的高度
+      let product = {};
+      product.image = this.slideshow[0];
+      product.title = this.goods.title;
+      product.desc = this.goods.desc;
+      product.price = this.goods.newPrice;
+      product.iid = this.detail;
+      product.trueprice = this.goods.realPrice;
+
+      //使用vuex把用户添加的购物车的那些东西都方法哦vuex数据中做保存
+      //用store中的actions中来调用方法
+      //this.$store.dispatch('addshop', product)
+      this.$store.commit("addshop", product).then(result => {
+        console.log(result);
+      });
+    }
+  },
+  //当页面只要一发生变化就执行函数updated中的内容的
+  //注意不只执行一次的
+  updated() {
+    /* //这里写这个代码也可以获取到各个组件对应的顶部的高度
             //但是有的时候因为图片加载慢的原因，导致获取的高度是不正确的
             this.tabbar = []
             //$el 就是拿到组件的根组件，offsetTop就是距离顶部的位置
@@ -257,77 +254,73 @@ export default {
             this.tabbar.push(this.$refs.commentInfo.$el.offsetTop)
             this.tabbar.push(this.$refs.goodss.$el.offsetTop)
             console.log(this.tabbar); */
-    }
-        
+  }
 };
 </script>
 
 <style scoped>
-  .divv{
-      position: relative;
-      z-index: 11;
-      background: #ffffff;
-      height: 150vh;
-     
-  }
+.divv {
+  position: relative;
+  z-index: 11;
+  background: #ffffff;
+  height: 150vh;
+}
 
-  .scrolll{
-      /* height: calc(100%); */
-      position: relative;
-     
-      height:100vh; 
-      overflow: hidden;
-     
-  }
-  .navbar{
-      position:fixed;
-      background: #ffffff;
-  }
-  .title{
-      
-      display:flex;
-  }
-  .titleItem{
-      flex:1;
-      font-size: 14px;
-      margin: 0px auto;
-      line-height: 44px;
-  }
-  .active{
-      color:red;
-  }
-  .left img{
-    position: relative;
-      top: 5px;
-      width: 30%;
-      height: 30%;
-  }
-  .scroll{
-      height: calc(100% - 44px);
-      overflow: hidden;
-      position: relative;
-      top: 44px;
-  }
-  .class{
-      position: relative;
-      top: 44px;
-      height: 30px;
-      overflow: hidden;
-  }
-  .images{
-      position: relative;
-      top: 70px;
-  }
-  .goodss{
-      position: relative;
-      top: 200px;
-  }
-  .basetop{
-      position: absolute;
-      z-index: 12;
-      top: 50%;
-      left: 70%;
-     width: 100px;
-     height: 100px;
-  }
+.scrolll {
+  /* height: calc(100%); */
+  position: relative;
+
+  height: 100vh;
+  overflow: hidden;
+}
+.navbar {
+  position: fixed;
+  background: #ffffff;
+}
+.title {
+  display: flex;
+}
+.titleItem {
+  flex: 1;
+  font-size: 14px;
+  margin: 0px auto;
+  line-height: 44px;
+}
+.active {
+  color: red;
+}
+.left img {
+  position: relative;
+  top: 5px;
+  width: 30%;
+  height: 30%;
+}
+.scroll {
+  height: calc(100% - 44px);
+  overflow: hidden;
+  position: relative;
+  top: 44px;
+}
+.class {
+  position: relative;
+  top: 44px;
+  height: 30px;
+  overflow: hidden;
+}
+.images {
+  position: relative;
+  top: 70px;
+}
+.goodss {
+  position: relative;
+  top: 200px;
+}
+.basetop {
+  position: absolute;
+  z-index: 12;
+  top: 50%;
+  left: 70%;
+  width: 100px;
+  height: 100px;
+}
 </style>
